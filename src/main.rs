18c9a2ddc47db8135c5abf37bfc5b6cddc6c641f
friendbear <friendbear@users.noreply.github.com>
2021-08-02 12:31:43 +0900

    datalink -> Ethernet

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::env;
-use pnet::datalink;
+extern crate pnet;
 #[macro_use]
 extern crate log;
 
+use std::env;
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
+
 mod packets;
+
+const WIDTH: usize = 20;
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
@@ -23,5 +35,34 @@ fn main() {
             .into_iter()
             .find(|iface| iface.name == *interface_name)
             .expect("Failed to get interface");
-    
+
+    /* [1]: データリンクのチャンネルを取得 */
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("Failed to create datalink channel {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(frame) => {
+                // 受信データからイーサネットフレームの構築
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        println!("Ipv4 packet...")
+                    }
+                    EtherTypes::Ipv6 => {
+                        println!("Ipv6 packet...")
+                    }
+                    _ => {
+                        info!("Not an IPv4 or IPv6 packet");
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to read: {}", e);
+            }
+        }
+    }
 }
