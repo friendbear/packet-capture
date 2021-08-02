c4e6ce4da81594dbc9644b6cebc7c68af517aa07
friendbear <friendbear@users.noreply.github.com>
2021-08-02 15:55:41 +0900

    ipv4, ipv6 -> TCP, UDP

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::Packet;
 
 mod packets;
+use packets::GettableEndPoints;
 
 const WIDTH: usize = 20;
 
@@ -50,10 +51,10 @@ fn main() {
                 let frame = EthernetPacket::new(frame).unwrap();
                 match frame.get_ethertype() {
                     EtherTypes::Ipv4 => {
-                        println!("Ipv4 packet...")
+                        ipv4_handler(&frame);
                     }
                     EtherTypes::Ipv6 => {
-                        println!("Ipv6 packet...")
+                        ipv6_handler(&frame);
                     }
                     _ => {
                         info!("Not an IPv4 or IPv6 packet");
@@ -65,4 +66,76 @@ fn main() {
             }
         }
     }
+}
+/**
+ * IPv4パケットを構築し次のレイアのハンドラを呼び出す
+ */
+fn ipv4_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv4Packet::new(ethernet.payload()) {
+        match packet.get_next_level_protocol() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            }
+            _ => {
+                info!("Not a TCP or UDP packet");
+            }
+        }
+    }
+}
+
+/**
+ * IPv6パケットを構築し次のレイアのハンドラを呼び出す
+ */
+fn ipv6_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv6Packet::new(ethernet.payload()) {
+        match packet.get_next_header() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            }
+            _ => {
+                info!("Not a TCP or UDP packet");
+            }
+        }
+    }
+}
+
+/**
+ * TCPパケットを構築する
+ */
+fn tcp_handler(packet: &GettableEndPoints) {
+    let tcp = TcpPacket::new(packet.get_payload());
+    if let Some(tcp) = tcp {
+        print_packet_info(packet, &tcp, "TCP");
+    }
+}
+
+/**
+ * UDPパケットを構築する
+ */
+fn udp_handler(packet: &GettableEndPoints) {
+    let udp = UdpPacket::new(packet.get_payload());
+    if let Some(udp) = udp {
+        print_packet_info(packet, &udp, "UDP")
+    }
+}
+
+/**
+ * アプリケーション層のデータをバイナリで表示する。
+ */
+fn print_packet_info(l3: &GettableEndPoints, l4: &GettableEndPoints, proto: &str) {
+
+    println!(
+        "Captured a {} packet from {}|{} to {}|{}\n",
+        proto,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination(),
+        l4.get_destination()
+    );
 }
